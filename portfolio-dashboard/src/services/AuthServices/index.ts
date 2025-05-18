c48112d529e94505d58dfedcbd26d1655aabd91e
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-05-18 16:22:19 +0600

    feature(api): added 2 api (loginUser,getCurrentUser) in services file

---
+++ portfolio-dashboard/src/services/AuthServices/index.ts
@@ -0,0 +1,45 @@
+'use server';
+
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+import { FieldValues } from "react-hook-form";
+
+export const loginUser = async (userData: FieldValues): Promise<any> => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/auth/login`, {
+      method: 'POST',
+      body: JSON.stringify(userData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const result = await res.json();
+
+    if (result?.success) {
+      (await cookies()).set('accessToken', result?.data?.accessToken);
+      (await cookies()).set('refreshToken', result?.data?.refreshToken);
+    }
+
+    return result;
+  } catch (error: any) {
+    return Error(error);
+  }
+};
+
+export const getCurrentUser = async (): Promise<any> => {
+  const accessToken = (await cookies()).get('accessToken')?.value;
+  let decodedData = null;
+
+  if (accessToken) {
+    decodedData = await jwtDecode(accessToken);
+    return decodedData;
+  } else {
+    return null;
+  }
+};
+
+export const logOut = async (): Promise<void> => {
+  (await cookies()).delete('accessToken');
+  (await cookies()).delete('refreshToken');
+};
