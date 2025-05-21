c50449cb29edcc2279102888c2f4d410087ac651
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-05-21 23:50:02 +0600

    updated

--- portfolio-dashboard/src/app/(dashboard)/(blog)/blogs/page.tsx
+++ portfolio-dashboard/src/app/(dashboard)/(blog)/blogs/page.tsx
@@ -6,7 +6,7 @@ export default async function BlogPage() {
 
   return (
     <div className=" py-8">
-      <h1 className="text-2xl font-bold mb-4">All Projects</h1>
+      <h1 className="text-2xl font-bold mb-4">All Blogs</h1>
        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:px-12 px-2">
           {data?.map((blog:any, index:number) => (
             <BlogCart key={index} blog={blog} />
